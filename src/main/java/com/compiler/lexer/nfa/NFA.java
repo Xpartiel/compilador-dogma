d5946c422d0572f6cc661a3b9fad507a6a16e9b6
Xpartiel <xparting@ciencias.unam.mx>
2025-08-31 11:53:54 -0600

    feat: NFA automatically flags end state as final

--- src/main/java/com/compiler/lexer/nfa/NFA.java
+++ src/main/java/com/compiler/lexer/nfa/NFA.java
@@ -26,6 +26,7 @@ public class NFA {
     public NFA(State start, State end) {
         this.startState = start;
         this.endState = end;
+        this.endState.isFinal = true;
     }
 
     /**
