91ad1970b9ec44247b4fcbbd7497948b9414830e
Xpartiel <xparting@ciencias.unam.mx>
2025-09-16 21:50:12 -0600

    feat: Token Type implemented

---
+++ src/main/java/com/compiler/lexer/Token.java
@@ -0,0 +1,24 @@
+package com.compiler.lexer;
+
+public class Token {
+    private final String type;
+    private final String lexeme;
+
+    public Token(String type, String lexeme) {
+        this.type = type;
+        this.lexeme = lexeme;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" +type+ ", " +lexeme+ '}';
+    }
+}
